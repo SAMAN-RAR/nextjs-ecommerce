8b8250d55188c12aede6d024cbfa0fb9bb6b47fa
saman.rar <samanfaraji1384@gmail.com>
2025-12-25 09:51:28 +0330

    cart fixed

--- app/_actions/cart.js
+++ app/_actions/cart.js
@@ -1,16 +1,16 @@
-"use server";
+'use server';
 
-import { auth } from "@/customerAuth";
-import db from "@/db/db";
-import { revalidatePath } from "next/cache";
+import { auth } from '@/customerAuth';
+import db from '@/db/db';
+import { revalidatePath } from 'next/cache';
 
 export const addToCart = async (productId) => {
   const session = await auth();
   if (!session?.user?.id) {
     const result = {
-      status: "error",
-      type: "authentication",
-      message: "Must authenticate first!",
+      status: 'error',
+      type: 'authentication',
+      message: 'Must authenticate first!',
     };
     return result;
   }
@@ -39,9 +39,9 @@ export const addToCart = async (productId) => {
 
   if (existingInCart) {
     const result = {
-      status: "error",
-      type: "cartError",
-      message: "This item is already in your cart!",
+      status: 'error',
+      type: 'cartError',
+      message: 'This item is already in your cart!',
     };
     return result;
   }
@@ -51,9 +51,9 @@ export const addToCart = async (productId) => {
   });
   if (existingInOrder) {
     const result = {
-      status: "error",
-      type: "orderError",
-      message: "You own this item!",
+      status: 'error',
+      type: 'orderError',
+      message: 'You own this item!',
     };
 
     return result;
@@ -66,26 +66,26 @@ export const addToCart = async (productId) => {
   });
 
   const result = {
-    status: "success",
-    type: "success",
-    message: "Added to Cart!",
+    status: 'success',
+    type: 'success',
+    message: 'Added to Cart!',
   };
-  revalidatePath("/profile/cart", "page");
-  revalidatePath("/profile", "page");
+  revalidatePath('/profile/cart', 'page');
+  revalidatePath('/profile', 'page');
   return result;
 };
 
 export const deleteProductFromCart = async (id) => {
   await db.cartItem.delete({ where: { id } });
-  revalidatePath("/profile/cart", "page");
-  revalidatePath("/profile", "page");
+  revalidatePath('/profile/cart', 'page');
+  revalidatePath('/profile', 'page');
 };
 
 export const getCartProducts = async () => {
   const session = await auth();
-  if (!session) throw new Error("Unauthenticated!");
+  if (!session) throw new Error('Unauthenticated!');
 
-  const cart = await db.cart.findUnique({
+  let cart = await db.cart.findUnique({
     where: { userId: session.user.id },
   });
 
