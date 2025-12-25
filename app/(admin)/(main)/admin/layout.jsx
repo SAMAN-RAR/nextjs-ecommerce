d615e708b9539cd47055bf0b9035c472e1077966
saman.rar <samanfaraji1384@gmail.com>
2025-12-25 10:13:50 +0330

    removing auth from layout

--- app/(admin)/(main)/admin/layout.jsx
+++ app/(admin)/(main)/admin/layout.jsx
@@ -1,26 +1,26 @@
-import { auth } from "@/auth";
-import { Nav, NavLink } from "@/components/Nav";
-import SignOutBtn from "@/components/SiginouBtn";
+// import { auth } from "@/auth";
+import { Nav, NavLink } from '@/components/Nav';
+import SignOutBtn from '@/components/SiginouBtn';
 
 const AdminLayout = async ({ children }) => {
-  const session = await auth();
+  // const session = await auth();
   return (
     <>
       <Nav>
         <h2>Asia Shop</h2>
-        <div className="p-3">
-          <NavLink href="/admin">Dashboard</NavLink>
-          <NavLink href="/admin/products">Products</NavLink>
-          <NavLink href="/admin/customers">Customers</NavLink>
-          <NavLink href="/admin/sales">Sales</NavLink>
+        <div className='p-3'>
+          <NavLink href='/admin'>Dashboard</NavLink>
+          <NavLink href='/admin/products'>Products</NavLink>
+          <NavLink href='/admin/customers'>Customers</NavLink>
+          <NavLink href='/admin/sales'>Sales</NavLink>
         </div>
-        <div className="flex">
+        <div className='flex'>
           <SignOutBtn>Signout</SignOutBtn>
         </div>
       </Nav>
 
-      <div className="container my-2 mx-auto p-4">
-        <h3 className="text-3xl p-4">{`Welcome ${session.user.username}`}</h3>
+      <div className='container my-2 mx-auto p-4'>
+        {/* <h3 className="text-3xl p-4">{`Welcome ${session.user.username}`}</h3> */}
         {children}
       </div>
     </>
--- db/db.js
+++ db/db.js
@@ -1,9 +1,9 @@
-import { PrismaClient } from "../app/generated/prisma";
+import { PrismaClient } from '@/app/generated/prisma';
 
 const globalForPrisma = global;
 
 const db = globalForPrisma.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db;
 
 export default db;
